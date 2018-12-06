898b5e32561f1e424d04d8dd68418ea845d96d99
AHMAD FATHULLAH BIN MUSA <44662857+ahmadfathullah@users.noreply.github.com>
2018-12-07 05:55:35 +0800

    Update newserver.java

--- Projekt/newserver.java
+++ Projekt/newserver.java
@@ -1,30 +1,20 @@
-import java.io .IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.net.*;
+import java.io.*;
+import.java.util.*;
 
 public class newserver {
 
-public static void main(String[] args) throws IOException {
-ServerSocket ss = new ServerSocket(43454);
+public static void main(String[] args) throws Exception {
+ServerSocket ss = new ServerSocket(4347);
 while (true) {
-socket s = ss.accept();
+System.out.println("Waiting for Connection ...");
+socket sock = ss.accept();
 System.out.println("New socket " + s.getLocalSocketAddress() + " " + s.getRemoteSocketAddress());
 
-Calendar cal = Calendar.getInstance();
-Date date=cal.getTime();
-DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyy hh:mm:ss");
-String formattedDate=dateFormat.format(Date);
-
-try(Writer out = new OutputStreamWriter(s.getOutputStream())) {
-out.writer(formattedDate);
-out.flush();
-}
+DataOutputStream out= new DataOutputStream(sock.getOutputStream());
+out.writeBytes("server Date: "+(new Date()).toString() + "\n");
+out.close();
+sock.close();
 }
 }
 }
