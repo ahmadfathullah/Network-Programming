81df5c1f0bfaf0c50feb3e445690576d53c2912a
AHMAD FATHULLAH BIN MUSA <44662857+ahmadfathullah@users.noreply.github.com>
2019-03-20 00:17:03 +0800

    Create ClientChat2.java

---
+++ ClientServerChat/ClientChat2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+public class ClientChat2
+{
+  public static void main(String[] args) throws Exception
+  {
+     Socket sock = new Socket("192.168.40.128", 2080);
+                       
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+             
+     OutputStream ostream = sock.getOutputStream(); 
+     PrintWriter pwrite = new PrintWriter(ostream, true);
+
+     InputStream istream = sock.getInputStream();
+     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+
+     System.out.println("Start the chitchat, type and press Enter key");
+
+     String receiveMessage, sendMessage;               
+     while(true)
+     {
+        sendMessage = keyRead.readLine(); 
+        pwrite.println(sendMessage);      
+        pwrite.flush();             
+        if((receiveMessage = receiveRead.readLine()) != null) 
+        {
+            System.out.println(receiveMessage); 
+        }         
+      }               
+    }                    
+}                        
